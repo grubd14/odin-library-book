4524e827bf25c630808272f43a3eb9b9c2279128
grubd14 <83056016+grubd14@users.noreply.github.com>
2024-12-10 21:26:56 +0545

    dynamically creating books from array

--- script.js
+++ script.js
@@ -15,13 +15,15 @@ function addBookToLibrary(bookTitle, bookAuthor, bookPages, bookRead) {
 }
 
 addBookToLibrary("Hello", "World", 322, true);
+addBookToLibrary("Wordl", "Thompson", 242, true);
+addBookToLibrary("Hola", "Brian", 121, true);
 
 //Dialog box
 const dialog = document.querySelector("dialog");
 const showButton = document.querySelector("dialog + button");
 const closeButton = document.querySelector("dialog button");
 
-//opens the dialof box 
+//opens the dialog box 
 showButton.addEventListener("click", () => {
     dialog.showModal();
 })
@@ -30,7 +32,35 @@ closeButton.addEventListener("click", () => {
     dialog.close();
 })
 
+//prevent submit button from submittin to the server yb default
+const submitButton = document.querySelector("#formSubmit");
+submitButton.addEventListener("click", (event) => {
+    event.preventDefault();
+})
 
-console.log(dialog)
+
+//looping through each items to create a book card
+myLibrary.forEach((book) => {
+    bookCard(book);
+})
+
+//card view Each book
+function bookCard(myLibrary) {
+    const bookCardSection = document.querySelector(".bookCardSection");
+    const bookCardDiv = document.createElement("div");
+    bookCardDiv.classList.add("bookCard");
+    bookCardDiv.innerHTML += 
+    `
+        <h2>Books</h2>
+        <p>Book Name: ${myLibrary.bookTitle}</p>
+        <p>Book Author: ${myLibrary.bookAuthor} </p>
+        <p>Book Pages: ${myLibrary.bookPages}</p>
+        <p>Book Read Status: ${myLibrary.bookRead}</p>
+    `
+    bookCardSection.appendChild(bookCardDiv);
+}
+
+
+
 
 
